aff2052b637fdba39c4f8296f35d7249ee693302
Davit <narek.aproyan.bw@gmail.com>
2017-09-25 19:13:53 +0400

    Start

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,111 @@
+const path = require('path')
+const fs = require('fs')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
+
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack')
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  entry: {
+    app: './src/main.js',
+    'fa':   'font-awesome-sass-loader!./build/webpack.font-awesome.config.js',
+    'twbs': 'bootstrap-loader'
+  },
+  output: {
+    path: config.build.assetsRoot,
+    filename: '[name].js',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': resolve('src'),
+    },
+    symlinks: false
+  },
+  module: {
+    rules: [
+      /*
+       * Bootstrap 4 loader
+       */
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: 'imports-loader?jQuery=jquery'
+      },
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [resolve('src'), resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter')
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ['popper.js', 'default'],
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Util: "exports-loader?Util!bootstrap/js/dist/util"
+    }),
+    new webpack.LoaderOptionsPlugin({
+      postcss: [autoprefixer],
+    }),
+  ]
+}
---
+++ build/webpack.font-awesome.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // fontAwesomeCustomizations: "./_font-awesome.config.scss",
+  styles: {
+    "mixins": true,
+    "bordered-pulled": false,
+    "core": true,
+    "fixed-width": true,
+    "icons": true,
+
+    "larger": true,
+    "path": true,
+    "animated": true,
+    "stacked": false,
+    "rotated-flipped": false,
+    "larger": true,
+    "list": true,
+  }
+};
